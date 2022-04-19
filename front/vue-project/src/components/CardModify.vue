da48a36af0cdd8844cdc876dfea7bf364b775d76
romainmltr <romain_malaterre@hotmail.fr>
2022-04-19 19:56:23 +0200

    add part

---
+++ front/vue-project/src/components/CardModify.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" rel="stylesheet/scss">
+/* write SCSS here */
+
+</style>
